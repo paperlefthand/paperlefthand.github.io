183cfab7c5375bbe8925389ec61d2f94e0ba2d68
paperlefthand <being.paperlefthand@gmail.com>
2018-12-02 22:25:51 +0900

    レイアウト

--- src/components/outside.js
+++ src/components/outside.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 export default() => (
     <div>
     <h2>-OutSide-</h2>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,25 +2,14 @@ import React from 'react';
 import Link from 'gatsby-link';
 import yellow from './yellow.jpg';
 import Outside from "../components/outside";
+import Products from "../components/products";
 
 const IndexPage = () => (
   <div>
     <img src={yellow} width="100" height="100"></img>
     
   <section>
-  <h2>-Products-</h2>
-  <ul>
-      <li>スカラネットスクショくん (<a href="https://github.com/paperlefthand/Scholarnet" target="_blank">GitHub</a>)</li>
-      <li>ClickPost (<a href="https://github.com/paperlefthand/click_post" target="_blank">GitHub</a>)</li>
-      <li>オレ達の妹シリーズ</li>
-          <ul>
-              <li>流行に敏感な妹bot (<a href="https://twitter.com/sister_trend" target="_blank">Twitter</a>)</li>
-              <li><Link to="/page-2/">グルメ通な妹bot </Link> (LINEbot)</li>
-              <li><Link to="/page-3/">面倒見がよい反面お節介でもある妹bot </Link> (LINEbot)</li>
-
-          </ul>
-  </ul>
-
+      <Products />
   </section>
   <section>
       <Outside />
